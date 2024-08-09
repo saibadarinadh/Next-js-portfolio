989c452aab3efa723a73c64cfb7afe07cebf016c
Noah Gomes <noahgomes02@yahoo.com>
2024-08-09 20:45:04 +0100

    refactor: hero

---
+++ lib/consts.tsx
@@ -0,0 +1,21 @@
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
+export const HERO_LINKS = [
+  {
+    icon: <FaGithub size={24} />,
+    src: "https://github.com/NoahGdev",
+  },
+  {
+    icon: <FaLinkedin size={24} />,
+    src: "https://linkedin.com/in/noah-gomes",
+  },
+  {
+    icon: <FaTwitter size={24} />,
+    src: "https://x.com/NoxhDevs",
+  },
+  {
+    icon: <IoDocumentTextOutline size={24} />,
+    src: "/resume",
+  },
+];
