0b255652f2f75c63aeb4bd1a0ad01031d3019e26
Noah Gomes <noahgomes02@yahoo.com>
2024-10-22 02:05:38 +0100

    refactor: new nav links

--- components/Navbar/Item.tsx
+++ components/Navbar/Item.tsx
@@ -1,22 +1,32 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { useCallback } from 'react';
 
-export const Item = ({ children, className }: Readonly<{ children: React.ReactNode; className?: string }>) => {
-  const path = usePathname();
-  const active =
-    path === children!.toString().toLowerCase().split(' ').join('-') || (path === '/' && children === 'Home');
+export const Item = ({
+  children,
+  href,
+  className,
+}: Readonly<{ children: React.ReactNode; href: string; className?: string }>) => {
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault();
+      const element = document.getElementById(href.replace('/', '').replace('#', ''));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    },
+    [href],
+  );
 
   return (
-    <div
-      className={cn(
-        'font-semibold transition-all duration-300 hover:cursor-pointer hover:text-[#f0f0f0]',
-        className,
-        active && '',
-      )}
+    <Link
+      className={cn('font-semibold transition-all duration-300 hover:cursor-pointer hover:text-[#f0f0f0]', className)}
+      href={href}
+      onClick={handleClick}
     >
       {children}
-    </div>
+    </Link>
   );
 };
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,12 +4,11 @@ import { Item } from './Item';
 const Navbar: React.FC = () => {
   return (
     <nav className="-mb-14 flex items-center justify-center pt-8 text-[#e6e6e6]">
-      <div className="w-fit rounded-full border border-[#161616] bg-[#3b3b3b] bg-opacity-50 shadow-lg backdrop-blur-xl">
+      <div className="z-10 w-fit rounded-full border border-[#161616] bg-[#3b3b3b] bg-opacity-50 shadow-lg backdrop-blur-xl">
         <div className="flex flex-row items-center space-x-12 px-5 py-4">
-          <Item>Home</Item>
-          <Item>About</Item>
-          <Item>Previous Work</Item>
-          <Item>Contact</Item>
+          <Item href={'/'}>Home</Item>
+          <Item href={'/#about'}>About</Item>
+          <Item href={'/#prev-work'}>Previous Work</Item>
         </div>
       </div>
     </nav>
